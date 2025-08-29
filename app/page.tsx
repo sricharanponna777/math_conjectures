47338dd68b9ca175629b72eb0ef0f46d26f5bd36
Sricharan Ponna <sricharanponna777@gmail.com>
2025-08-29 15:32:23 +0100

    Improved look of Buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
 
 function PerfectNumbers() {
   return (
-    <div>
-      <Link href="/mersenne">Mersenne Perfect Numbers</Link>
-      <Link href="/collatz">Collatz Conjecture</Link>
+    <div className='flex flex-col gap-4 p-6'>
+      <Link href="/mersenne">
+        <Button className='bg-blue-500 hover:underline text-white rounded'>Mersenne Perfect Numbers</Button>
+      </Link>
+      <Link href="/collatz">
+        <Button className='bg-blue-500 hover:underline text-white rounded'>Collatz Conjecture</Button>
+      </Link>
     </div>
   )
 }
